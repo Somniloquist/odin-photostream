ac2c239a086e283724fdd9b0ad11b3d23f8203f0
Robin Unger <robin.e.unger@gmail.com>
2019-11-14 15:20:40 -0600

    Search and display photos for a flickr ID

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,23 @@
 class StaticPagesController < ApplicationController
+  require "flickraw"
+  FlickRaw.api_key = ENV["FLICKRAW_API_KEY"]
+  FlickRaw.shared_secret = ENV["FLICKRAW_SHARED_SECRET"]
+
+  def home
+    @flickr = FlickRaw::Flickr.new
+    if params[:search]
+      begin
+        @photos = flickr.people.getPublicPhotos(api_key: FlickRaw.api_key, user_id: search_params[:id])
+      rescue
+        flash.now[:danger] = "User not found"
+        render :home
+      end
+    end
+  end
+
+  private
+    def search_params
+      params.require(:search).permit(:id)
+    end
+
 end
